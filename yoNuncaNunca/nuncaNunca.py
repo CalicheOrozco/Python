a4af83e0589fc6a6656fb30f2bc9d71a8183afa5
Caliche Orozco <caliche2da1@gmail.com>
2020-10-17 03:03:02 -0500

    Juego de yo NUNCA, NUNCA propio

---
+++ yoNuncaNunca/nuncaNunca.py
@@ -0,0 +1,21 @@
+import csv
+from random import randrange, randint
+
+
+def NuncaNuncaRandom(i):
+	#Crea la lista vacia
+	frase = []
+	#Abre el csv donde estan los yo nunca nunca
+	with open('yoNuncaNunca.csv') as File:
+	    reader = csv.DictReader(File, delimiter='.')
+	    #Recorre cada columna y la agrega a la lista
+	    for row in reader:
+
+	        frase.append(row)
+
+
+	    #retorna la frase del numero porporcionado
+	    return frase[i]['Frases']
+
+nuncanunca = NuncaNuncaRandom(randrange(0, 200))
+print(nuncanunca)
